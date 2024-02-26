c98ae73231941e82c11e71ceb307990ddea80e7e
RyanYoung72443 <sryoung72443@gmail.com>
2024-02-26 02:08:33 -0500

    functional calculator

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	const inflationRate = 2.5
+	var investmenAmmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmenAmmount)
+	fmt.Print("Investment years: ")
+	fmt.Scan(&years)
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	futureValue := investmenAmmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
